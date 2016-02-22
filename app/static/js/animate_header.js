11c55c07ee16d7790f061e01268b2a1de5590cd1
sthoma <sthoma4@gmail.com>
2016-02-22 15:47:44 -0700

    Mega overhaul. Ditch grid for flex. Better Mobile. SASS & Jade.

--- app/static/js/animate_header.js
+++ app/static/js/animate_header.js
@@ -1,17 +1,26 @@
-$(document).ready(function() {
-    var $head = $('.header');
-    animClassDown = $head.data('animateDown');
-    animClassUp = $head.data('animateUp' );
-    if( $('.story').css('margin-top') !== '70px') {
-        $('.story').waypoint( function( direction ) {
-            if( direction === 'down' && animClassDown ) {
-                $head.attr('class', 'header ' + animClassDown);
-            }
-            else if( direction === 'up' && animClassUp ){
-                $head.attr('class', 'header ' + animClassUp);
-            }
-        }, { offset: '10%' });
-    } else {
+var header_resize = function() {
+  var $head = $('.header');
+  animClassDown = $head.data('animateDown');
+  animClassUp = $head.data('animateUp' );
+  if( $('body').css('bottom') == '0px') {
+
+    $head.attr('class', 'header ' + animClassUp);
+
+    $('.story').waypoint( function( direction ) {
+      if( direction === 'down' && animClassDown ) {
         $head.attr('class', 'header ' + animClassDown);
-    }
-});
+      }
+      else if( direction === 'up' && animClassUp ){
+        $head.attr('class', 'header ' + animClassUp);
+      }
+    }, { offset: '10%' });
+  }
+  else {
+    $head.attr('class', 'header ' + animClassDown);
+    $('.story').waypoint('destroy');
+  }
+};
+
+$(document).ready(function() { header_resize(); });
+$(window).resize(function() { header_resize(); });
+
